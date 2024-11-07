378eaa555d07ac3d59d992b1b4f81be31be31b95
RhodCoding <joewee321@gmail.com>
2024-11-07 16:50:09 +0800

    Add Nav Bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,14 @@
 	let { children } = $props();
 </script>
 
+<main>
+	<nav>
+		<ul>
+			<li><a href="/">home</a></li>
+			<li><a href="/shop">shop</a></li>
+			<li><a href="/contact">contact</a></li>
+			<li><a href="/visit-us">visit us</a></li>
+		</ul>
+	</nav>
 {@render children()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,1 @@
-<script lang="ts">
-	import formValidator from "$lib/formValidator";
-	import { Card, Input } from "flowbite-svelte";
-    
-
-	let formValues = $state({ name: "", age: 0 });
-    let errors = $derived(formValidator(formValues));
-</script>
-
-<p>Testing ko lang</p>
+<p>Testing ko lang ewan</p>
