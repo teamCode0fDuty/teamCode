18278132de42f3c47f8c29d55fec10606a11d5c4
RhodCoding <joewee321@gmail.com>
2024-11-07 16:00:13 +0800

    Create Svelte 5

---
+++ project2/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import formValidator from "$lib/formValidator";
+	import { Card, Input } from "flowbite-svelte";
+    
+
+	let formValues = $state({ name: "", age: 0 });
+    let errors = $derived(formValidator(formValues));
+</script>
+
+<p>Testing ko lang</p>
